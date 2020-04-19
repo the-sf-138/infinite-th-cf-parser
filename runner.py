5a13992c7e991bbfaa828d7166d3d5ce400d6036
TimothySanchirico <timothy.sanchirico@gmail.co>
2020-04-19 15:10:35 -0500

    fixing compiler error reporting for cpp, adding haskell option

--- runner.py
+++ runner.py
@@ -2,32 +2,44 @@ import os
 import subprocess
 import glob
 
+import log
+
 # Language specs
 # only python3 cuz fuck py2
 class Cpp: pass
 class Python: pass
+class Haskell: pass
 
 FILE_TYPES = {
-    Cpp    : [ ".C", ".cc", ".cpp" ],
-    Python : [ ".py" ]
+    Cpp     : [ ".C", ".cc", ".cpp" ],
+    Python  : [ ".py" ],
+    Haskell : [ ".hs" ],
     }
 
-TO_FILE_TYPE = { f : t for t, fs in FILE_TYPES.items() for f in fs }
+TO_FILE_TYPE     = { f : t for t, fs in FILE_TYPES.items() for f in fs }
+VALID_FILE_TYPES = set([f.split(".")[-1] for t, fs in FILE_TYPES.items() for f in fs ])
 
 def get_executable(type, problem, fname):
     if type == Cpp:
         cmd = f"g++ -std=c++14 {fname} -o {problem}"
         try:
-            subproccess.run(cmd.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
+            subprocess.run(cmd.split(), check=True)
             return f"./{problem}"
-        except subproccess.CalledProcessError:
+        except subprocess.CalledProcessError as e:
+            log.error(str(e))
             return None
+    elif type == Haskell:
+        return f"runhaskell {fname}"
     elif type == Python:
-            return f"python3 {fname}"
+        return f"python3 {fname}"
+    else:
+        log.error("Invalid type {type} {problem} {fname}")
 
 def get_filename(problem):
     files = os.listdir()
-    candidates = [ f for f in files if f.lower().startswith(problem.lower() + ".") and not f.endswith("~")]
+    candidates = [ f for f in files if f.lower().startswith(problem.lower() + ".")
+                   and not f.endswith("~")
+                   and (f.lower().split(".")[-1] in VALID_FILE_TYPES) ]
     assert len(candidates) == 1, ", ".join(candidates)
     return candidates[0]
 
